4d46ea93d55c3f441773216bca25aada3717d70f
irakl <irakliberdzena314@gmail.com>
2025-06-30 01:24:11 +0400

    have some stuff to do

---
+++ FormBuilder/Migrations/20250629211857_asdhahwdawd.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FormBuilder.Migrations
+{
+    /// <inheritdoc />
+    public partial class asdhahwdawd : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Tags",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedAt",
+                value: new DateTime(2025, 6, 30, 1, 18, 56, 779, DateTimeKind.Local).AddTicks(6455));
+
+            migrationBuilder.UpdateData(
+                table: "Tags",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedAt",
+                value: new DateTime(2025, 6, 30, 1, 18, 56, 779, DateTimeKind.Local).AddTicks(6466));
+
+            migrationBuilder.UpdateData(
+                table: "Tags",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreatedAt",
+                value: new DateTime(2025, 6, 30, 1, 18, 56, 779, DateTimeKind.Local).AddTicks(6467));
+
+            migrationBuilder.UpdateData(
+                table: "Tags",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "CreatedAt",
+                value: new DateTime(2025, 6, 30, 1, 18, 56, 779, DateTimeKind.Local).AddTicks(6468));
+
+            migrationBuilder.UpdateData(
+                table: "Tags",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "CreatedAt",
+                value: new DateTime(2025, 6, 30, 1, 18, 56, 779, DateTimeKind.Local).AddTicks(6469));
+
+            migrationBuilder.UpdateData(
+                table: "Tags",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "CreatedAt",
+                value: new DateTime(2025, 6, 30, 1, 18, 56, 779, DateTimeKind.Local).AddTicks(6470));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Tags",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedAt",
+                value: new DateTime(2025, 6, 30, 1, 1, 28, 660, DateTimeKind.Local).AddTicks(9638));
+
+            migrationBuilder.UpdateData(
+                table: "Tags",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedAt",
+                value: new DateTime(2025, 6, 30, 1, 1, 28, 660, DateTimeKind.Local).AddTicks(9652));
+
+            migrationBuilder.UpdateData(
+                table: "Tags",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreatedAt",
+                value: new DateTime(2025, 6, 30, 1, 1, 28, 660, DateTimeKind.Local).AddTicks(9654));
+
+            migrationBuilder.UpdateData(
+                table: "Tags",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "CreatedAt",
+                value: new DateTime(2025, 6, 30, 1, 1, 28, 660, DateTimeKind.Local).AddTicks(9655));
+
+            migrationBuilder.UpdateData(
+                table: "Tags",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "CreatedAt",
+                value: new DateTime(2025, 6, 30, 1, 1, 28, 660, DateTimeKind.Local).AddTicks(9656));
+
+            migrationBuilder.UpdateData(
+                table: "Tags",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "CreatedAt",
+                value: new DateTime(2025, 6, 30, 1, 1, 28, 660, DateTimeKind.Local).AddTicks(9657));
+        }
+    }
+}
